b10e494f7c47e29dd983467de4d1ab77fd693f4a
noriko1599 <sminko1599@gmail.com>
2020-12-23 01:07:32 +0700

    category view

--- backend/app.js
+++ backend/app.js
@@ -7,13 +7,25 @@ const app = express();
 const productRoutes = require('./product/productRoutes');
 const cartRoutes = require('./cart/cartRoutes');
 
-app.use(express.static('public'))
+app.use((req, res, next) => {
+    res.set('Access-Control-Allow-Origin', 'https://jasmine-ecommerce.web.app');
+    res.set('Access-Control-Allow-Credentials', 'true');
+
+    if (req.method === 'OPTIONS') {
+        // Send response to OPTIONS requests
+        res.set('Access-Control-Allow-Methods', 'GET');
+        res.set('Access-Control-Allow-Headers', 'Authorization');
+        res.set('Access-Control-Max-Age', '3600');
+        res.status(204).send('');
+    } else {
+        next()
+    }
+})
 
 app.use(logger('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
 
 app.use('/products', productRoutes);
 app.use('/carts', cartRoutes);
