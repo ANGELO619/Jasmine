497a9c4106080504b244642f3a5a9ba78971b757
noriko1599 <sminko1599@gmail.com>
2020-12-22 22:29:34 +0700

    add servicer account

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 
-admin.initializeApp();
+var serviceAccount = require("./jasmine-ecommerce-firebase-adminsdk-gikep-c0fa9562d4.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 const app = require('./app');
 
